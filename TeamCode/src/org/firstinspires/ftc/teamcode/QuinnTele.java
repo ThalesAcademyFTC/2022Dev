e82254808d7cc4d9cf0e19f725365a142d138f1f
ethan davis <ethan.davis26@thalesacademy.org>
2022-08-19 17:10:48 -0400

    my square, triangle, and hexagon code

--- TeamCode/src/org/firstinspires/ftc/teamcode/QuinnTele.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/QuinnTele.java
@@ -23,10 +23,10 @@ public class QuinnTele extends OpMode {
 
 
         //In the gap below would normally be where you would create if statements for buttons
-        if (gamepad1.y) {
-            robot.moveRight(0.5);
+        if (gamepad1.dpad_left){
+            robot.moveLeft(10);
         }
-        else robot.moveLeft(0.5);
+
 
         //MOVEMENT
         //First, we want to make the robot rest if the gamepad is not being touched
