d60c4fe7a8d9be517595e2caf810889830e73561
AlexDaCatcher <alexander.larson-dick27@thalesacademy.org>
2022-08-19 16:46:33 -0400

    Short tele simulation test

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/AlexTeleTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Tele")
+//@Disabled
+public class AlexTeleTest extends OpMode {
+
+    private Spark robot;
+
+    @Override
+    public void init() {
+
+        //This code initializes the drivetrain. Make sure that you have the right drivetrain selected!
+        robot = new Spark(this, Spark.Drivetrain.MECHANUM);
+    }
+
+    @Override
+    public void loop() {
+        //This nested if statement is used to make the robot move using the left joystick.
+        // It works for every drive train, as long as it is set up in Anvil!
+
+
+        //In the gap below would normally be where you would create if statements for buttons
+        if (gamepad1.x) {
+            robot.moveForward(1);
+        }
+
+        //MOVEMENT
+        //First, we want to make the robot rest if the gamepad is not being touched
+
+
+        //If the gamepad is NOT at rest, then we want to see what we need to do.
+        //If the gamepad is NOT at rest, then we want to see what we need to do.
+
+
+        //This is the code used to run the movement for Tank drivetrain.
+        robot.mechanumMovT(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+    }
+}
